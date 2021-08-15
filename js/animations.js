1da5e4567a973dfd5f048755048864ebf29b08c8
Alejandro Diaz <alejandro.diaz@tektonlabs.com>
2021-08-15 18:22:48 -0500

    first commit

---
+++ js/animations.js
@@ -0,0 +1,24 @@
+var animation1 = bodymovin.loadAnimation({
+    container: document.getElementById('lottie1'), 
+    path: 'animations/section1.json', 
+    renderer: 'svg', 
+    loop: true, 
+    autoplay: true, 
+});
+
+var animation2 = bodymovin.loadAnimation({
+    container: document.getElementById('lottie2'), 
+    path: 'animations/section2.json', 
+    renderer: 'svg', 
+    loop: true, 
+    autoplay: true, 
+});
+
+var animation3 = bodymovin.loadAnimation({
+    container: document.getElementById('lottie3'), 
+    path: 'animations/section3.json', 
+    renderer: 'svg', 
+    loop: true, 
+    autoplay: true, 
+});
+
---
+++ js/custom.js
@@ -0,0 +1,8 @@
+$('.js-tilt').tilt({
+    glare: true,
+    maxGlare: .1,
+    maxTilt: 10,
+    perspective: 1000,
+    scale: 1.05,
+    easing: "cubic-bezier(.03,.98,.52,.99)",
+});
